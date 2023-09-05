af101b17f298471ac4fac207afa21495e22d2b47
syahrandywaskito <syahrandywaskito666@gmail.com>
2023-09-05 10:41:11 +0700

    add include for dark toggle and menu user

--- resources/views/admin/report/komputer.blade.php
+++ resources/views/admin/report/komputer.blade.php
@@ -29,39 +29,13 @@
           </a>
         </div>
         <div class="flex items-center">
-            <div class="flex items-center ml-3">
-              <div>
-                <button type="button" class="flex text-sm bg-white rounded-full focus:ring-4 focus:ring-gray-300" aria-expanded="false" data-dropdown-toggle="dropdown-user">
-                  <span class="sr-only">Open user menu</span>
-                  <img class="w-8 h-8 p-1 rounded-full" src="{{asset('img/user.png')}}" alt="user photo">
-                </button>
-              </div>
-              <div class="z-50 hidden my-4 text-base list-none bg-gray-100 divide-y divide-gray-100 rounded shadow-md" id="dropdown-user">
-                <div class="px-4 py-3 font-roboto" role="none">
-                  <p class="text-sm text-gray-900" role="none">
-                    {{auth()->user()->name}}
-                  </p>
-                  <p class="text-sm font-medium text-gray-900 truncate" role="none">
-                    {{auth()->user()->email}}
-                  </p>
-                </div>
-                <ul class="py-1 font-roboto" role="none">
-                  <li>
-                    <a href="{{route('dashboard')}}" class="block px-4 py-2 text-sm text-gray-700 hover:translate-x-1 transition duration-200" role="menuitem">Dashboard</a>
-                  </li>
-                  <li>
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:translate-x-1 transition duration-200" role="menuitem">Settings</a>
-                  </li>
-                  <li>
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:translate-x-1 transition duration-200" role="menuitem">Earnings</a>
-                  </li>
-                  <li>
-                    <a href="{{route('logout')}}" class="block px-4 py-2 text-sm text-gray-700 hover:translate-x-1 transition duration-200" role="menuitem">Logout</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+
+          {{-- Dark toggle --}}
+          @include('components.dashboard.darktoggle')
+          
+          {{-- User Profile Menu --}}
+          @include('components.dashboard.userprofile')
+        </div>
       </div>
     </div>
   </nav>
