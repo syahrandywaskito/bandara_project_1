511b7ff866a070c7a536c59e1d39c72c3c596b73
Syahrandy Waskito <syahrandywaskito666@gmail.com>
2023-12-07 15:11:56 +0700

    Add : tambah component _data-search-button

---
+++ resources/views/components/dashboard/buttons/_date-search-button.blade.php
@@ -0,0 +1,10 @@
+<button
+  title="Cari berdasarkan tanggal"
+  type="submit"
+  class="p-4 ml-2 text-base font-medium text-center inline-flex items-center text-gray-50 bg-indigo-800 rounded-lg hover:bg-indigo-500 focus:z-10 focus:ring-2 focus:ring-indigo-800 transition duration-200 dark:bg-gray-100 dark:text-slate-800 dark:hover:bg-slate-600 dark:hover:text-gray-100 dark:border-gray-100 dark:focus:ring-slate-500"
+>
+  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+  </svg>
+  <span class="sr-only">Search</span>
+</button>
